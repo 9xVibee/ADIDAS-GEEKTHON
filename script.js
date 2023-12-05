4d8dbc07e2fd7363a8e0816766c759b17d072654
Abhay Panchal <abpanchal951357@gmail.com>
2023-12-05 14:02:50 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-Shery.textAnimate(".navbar .header-black-section" /* Element to target.*/, {
-  //Parameters are optional.
-  style: 1,
-  y: 10,
-  delay: 0.1,
-  duration: 2,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  multiplier: 0.1,
-});
-
 let lastScrollY = 0;
 
 window.addEventListener("scroll", function () {
